f8f9c03d8e79f285dba477756992eda0354c0fe1
Nim-os <10841304+Nim-os@users.noreply.github.com>
2020-05-10 20:38:57 -0400

    Add helper, design StarArea algo

--- Process.py
+++ Process.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageEnhance
 
-# Prepares image for FindStars
+# Prepares image by highlighting bright spots
 def PrepImage(img):
     # Secret sauce
     BRIGHTNESS = 0.005
@@ -20,29 +20,53 @@ def FindStars(img, threshold=50, low=40):
     rawImg = img.load()
 
     # Optimization, will only check every LINESKIP + 1 lines
-    LINESKIP = 2
-
+    LINESKIP = 1
 
     sizex,sizey = int(img.size[0]/LINESKIP),int(img.size[1]/LINESKIP)
 
+    # Filled with tuples of (starLeft, starUp, starRight, starDown)
     stars = []
 
     for x in range(0,sizex):
         for y in range(0,sizey):
             
-            brightness = sum(rawImg[x * LINESKIP, y * LINESKIP])/3
+            #brightness = sum(rawImg[x * LINESKIP, y * LINESKIP])/3
+
+            brightness = CalculateBrightness(rawImg[x * LINESKIP, y * LINESKIP])
 
             if(brightness > threshold):
+
+                # Have to check if star is already in list
                 stars.append(GetStarArea(rawImg, low, (x * LINESKIP, y * LINESKIP)))
 
+                #rawImg[x * LINESKIP, y * LINESKIP] = (255,0,0) # Check that stars are correctly being selected
+
+    print(stars)
 
     return 0
 
+# What is that photoshop thing that determines if it should grab the surrounding pixel ??
 def GetStarArea(rawImg, low, pos):
-    x,y = pos[0],pos[1]
+    pX,pY = pos[0],pos[1]
 
-    print("Star found at %d, %d" % (x, y))
+    #print("Star found at %d, %d" % (pX, pY))
 
     # Go in a cross to find max radius of point
+    # Go up until not apart of the star, multiply by -1 to flip to other side. And find the edge further down or up.
+    # Do same for right
+
+    x,y = pX,pY
+
+    # (x1, y1, x2, y2)
+    # (starLeft, starUp, starRight, starDown)
+    star = (0,0,0,0)
+
+    # Check vertical
+    
+        
+    # Check horizontal
 
     return 0
+
+def CalculateBrightness(pixel=(0,0,0)):
+    return sum(pixel)/3
