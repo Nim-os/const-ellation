e9e7b587ec3612c04fa9b7248ae3a18ad117c50d
Nim-os <10841304+Nim-os@users.noreply.github.com>
2020-05-10 20:08:33 -0400

    Fix PixelAccess obj bug

--- Process.py
+++ Process.py
@@ -15,15 +15,20 @@ def PrepImage(img):
 
 
 # Find bright spots in image with brightness at least threshold. Star positions will not expand past low
-def FindStars(rawImg=[(0,0,0)], threshold=50, low=40):
+def FindStars(img, threshold=50, low=40):
+
+    rawImg = img.load()
 
     # Optimization, will only check every LINESKIP + 1 lines
     LINESKIP = 2
 
+
+    sizex,sizey = int(img.size[0]/LINESKIP),int(img.size[1]/LINESKIP)
+
     stars = []
 
-    for x in len(rawImg[0]):
-        for y in len(rawImg):
+    for x in range(0,sizex):
+        for y in range(0,sizey):
             
             brightness = sum(rawImg[x * LINESKIP, y * LINESKIP])/3
 
@@ -36,7 +41,7 @@ def FindStars(rawImg=[(0,0,0)], threshold=50, low=40):
 def GetStarArea(rawImg, low, pos):
     x,y = pos[0],pos[1]
 
-    print("Star found at %d, %d" % x % y)
+    print("Star found at %d, %d" % (x, y))
 
     # Go in a cross to find max radius of point
 
